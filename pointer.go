ec0089cfc91da252a0872bf71fea4884f302ea3f
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-23 20:52:43 +0700

    khởi tạo 1 con trỏ trong Go.

--- pointer.go
+++ pointer.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//Khai báo 1 con trỏ
+	a := 100
+	var pointer *int
+	pointer = &a
+	fmt.Println(pointer)
+	fmt.Printf("%T", pointer)
+
+	fmt.Println()
+	b := 123
+	p2 := new(int)
+	p2 = &b
+	fmt.Println(p2)
+	fmt.Printf("%T", p2)
 }
